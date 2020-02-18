b92ff6e3c91cc1e008843cdb20f55e68b6f19de8
JamesT-35 <jamesgtatejr@gmail.com>
2020-02-18 14:24:02 -0600

    Finished product

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
-// my_name = 'Kevin'
-// my_id = '2112'
-// my_address = 'Memphis,TN'
+// let my_name = 'Kevin'
+// let my_id = '2112'
+// let my_address = 'Memphis,TN'
 // console.log(
 // `My name is ${my_name}. 
 //  My id is ${my_id}. 
 //  My address is ${my_address}.`)
 
-// constestant = "1_M_l337!"
-// score = "9001"
+// let constestant = "1_M_l337!"
+// let score = "9001"
 
 //console.log(`Congratulations ${constestant} !!, your score is ${score}.`)
 
 
-HomeTeam = prompt("What is your Home team?")
-Score = prompt(`How many points did the ${HomeTeam} score?`)
+let HomeTeam = prompt("What is your Home team?")
+let Score = prompt(`How many points did the ${HomeTeam} score?`)
 
+let VisitingTeam = prompt("What is your Visiting team?")
+let Score2 = prompt(`How many points did the ${VisitingTeam} score?`)
+
+if (Score < Score2) 
+{
+console.log( alert(
+    `Looks like the ${HomeTeam} have defeated the ${VisitingTeam}! 
+       
+    FINAL SCORE:
+    Home/ ${HomeTeam}: ${Score}   
+    Visitor/${VisitingTeam}: ${Score2} `) )
+    } 
+else if (Score == Score2)
+{
+  console.log( alert(
+    `Looks like the ${HomeTeam} have tied the ${VisitingTeam}! 
+    
+    FINAL SCORE:
+    Home/ ${HomeTeam}: ${Score}   
+    Visitor/${VisitingTeam}: ${Score2} ` ) )
+
+}
